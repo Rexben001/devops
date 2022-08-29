6ca26b0f222f770abbcdb55ef97778af80422d04
Rexben <rexben.rb@gmail.com>
2022-08-29 15:36:52 +0100

    update config

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -11,9 +11,9 @@ provider "aws" {
   region = "eu-west-2" # Setting my region to London. Use your own region here
 }
 
-resource "aws_ecr_repository" "my_first_ecr_repo" {
-  name = "my-first-ecr-repo" # Naming my repository
-}
+# resource "aws_ecr_repository" "my_first_ecr_repo" {
+#   name = "my-first-ecr-repo" # Naming my repository
+# }
 
 resource "aws_ecs_cluster" "my_cluster" {
   name = "my-cluster" # Naming the cluster
